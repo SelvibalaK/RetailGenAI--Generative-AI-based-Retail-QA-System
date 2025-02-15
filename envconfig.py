3043a8555d780dee3d63fc38a16b0edb42a711fe
Selvibala K <147035489+SelvibalaK@users.noreply.github.com>
2025-02-16 00:43:42 +0530

    Add files via upload

---
+++ envconfig.py
@@ -0,0 +1,10 @@
+GOOGLE_API_KEY = 'google_api_key'
+DB_CONFIG = {
+    "user": "root",
+    "password": "root",
+    "host": "localhost",
+    "database": "atliq_tshirts"
+}
+
+TEMPERATURE = 0.2
+MODEL_NAME = "gemini-pro"
